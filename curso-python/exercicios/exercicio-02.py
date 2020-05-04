431aad7a998fabc9b56df7b5d24e016ee4300c93
thiagoAlexandre3 <thiagodev3@gmail.com>
2020-05-04 17:06:51 -0300

    Adicionando o curso de Python

---
+++ curso-python/exercicios/exercicio-02.py
@@ -0,0 +1,5 @@
+print('====== Desafio 2 ======');
+nascDia = input('Dia: ');
+nascMes = input('Mês: ');
+nascAno = input('Ano: ');
+print('Voce nasceu no dia',nascDia, 'de', nascMes, 'de', nascAno, '. Correto?');
---
+++ curso-python/exercicios/exercicio-03.py
@@ -0,0 +1,4 @@
+n1 = input('Digite um número: ');
+n2 = input('Digite outro: ');
+soma = n1 + n2;
+print('A soma é: ', soma);
