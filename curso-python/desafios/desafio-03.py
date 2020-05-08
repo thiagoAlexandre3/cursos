67a94d212cbf8d497a992835e95c40fd7834e308
thiagoAlexandre3 <thiagodev3@gmail.com>
2020-05-08 19:57:07 -0300

    Consertando o erros no arquivo desafio do curso de Python

--- curso-python/desafios/desafio-03.py
+++ curso-python/desafios/desafio-03.py
@@ -1,4 +1,4 @@
-n1 = input('Digite um número: ');
-n2 = input('Digite outro: ');
+n1 = int(input('Digite um número: '));
+n2 = int(input('Digite outro: '));
 soma = n1 + n2;
 print('A soma é: ', soma);
