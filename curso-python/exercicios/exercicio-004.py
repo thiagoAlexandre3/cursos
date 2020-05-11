80d054bff6b921331f495ad522ea0c3c18d17213
thiagoAlexandre3 <thiagodev3@gmail.com>
2020-05-10 22:34:47 -0300

    Consertanto o exercicio-004 do curso de Python

--- curso-python/exercicios/exercicio-004.py
+++ curso-python/exercicios/exercicio-004.py
@@ -1,9 +1,10 @@
 leitura = input('Digite algo: ');
-print('O valor digitado é do tipo ', type(leitura));
-print('Obs.: Será respondido como, true para sim, e false para não!');
-print('É numero: ', leitura.isnumeric())
-print('É string: ', leitura.isalpha());
-print('É alfanumerico: ', leitura.isalnum());
-print('É spaço: ', leitura.isspace());
-print('Está em caixa alta: ', leitura.isupper());
-print('Está em caixa baixa: ', leitura.islower());
+print('O valor digitado é do tipo, ', type(leitura));
+print('É um numero? ', leitura.isnumeric())
+print('É alfabético? ', leitura.isalpha());
+print('É alfanumerico? ', leitura.isalnum());
+print('Contem apenas espaços? ', leitura.isspace());
+print('Está tudo em maisculas? ', leitura.isupper());
+print('Está tudo em minusculas? ', leitura.islower());
+#Faltava apenas a próxima função
+print('Está capitalizado? ', leitura.istitle());
