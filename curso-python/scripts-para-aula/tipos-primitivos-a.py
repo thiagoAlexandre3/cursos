6271289c4a6997fbdd047649ec2627c81c4b8b74
thiagoAlexandre3 <thiagodev3@gmail.com>
2020-05-10 22:04:47 -0300

    Criando arquivos da aula 06 do curso de Python

---
+++ curso-python/scripts-para-aula/tipos-primitivos-a.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número: '));
+n2 = int(input('Digite outro: '));
+soma = n1 + n2;
+print("A soma de {} + {} vale, {}!".format(n1, n2, soma));
---
+++ curso-python/scripts-para-aula/tipos-primitivos-b.py
@@ -0,0 +1,15 @@
+n = input("Digite um valor: ");
+print(type(n));
+#mostra qual o tipo da variavel
+#algumas funções:
+
+print(n.isnumeric());
+#retorna o valor boleano true, se der para converter essa string, para um número
+print(n.isalpha());
+#retorna o valor boleano true, se a variavel for composta de apenas letras
+print(n.isalnum());
+#retorna o valor boleano true, se a variavel for composta por letras ou numeros
+print(n.isupper());
+#retorna o valor boleano true, se a variavel for composta por letras maisculas
+print(n.islower());
+#retorna o valor boleano true, se a variavel for composta por letras minusculas
