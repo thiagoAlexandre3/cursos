9563d632752478220e806b0dad2808148355c7cb
thiagoAlexandre3 <thiagodev3@gmail.com>
2020-05-10 23:27:46 -0300

    Resolvendo o exercicio-006 do curso de Python

---
+++ curso-python/exercicios/exercicio-006.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '));
+dobro = n1 * 2;
+triplo = n1 * 3;
+raiz = n1 **(1/2);
+print('O dobro de {}, é {}, o triplo, é {}, e sua raiz quadrada é igual a: {}'.format(n1, dobro, triplo, raiz));
