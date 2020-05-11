87a2ce269ce3520eea4dd86a473bfd65eabd3eac
thiagoAlexandre3 <thiagodev3@gmail.com>
2020-05-11 11:40:28 -0300

    Resolvendo exercicio-008 do curso de Python

--- curso-python/exercicios/exercicio-008.py
+++ curso-python/exercicios/exercicio-008.py
@@ -1,12 +1,4 @@
-n1 = int(input('Digite um numero: '));
-print('{} X 0 ='.format(n1), n1 * 0);
-print('{} X 1 ='.format(n1), n1 * 1);
-print('{} X 2 ='.format(n1), n1 * 2);
-print('{} X 3 ='.format(n1), n1 * 3);
-print('{} X 4 ='.format(n1), n1 * 4);
-print('{} X 5 ='.format(n1), n1 * 5);
-print('{} X 6 ='.format(n1), n1 * 6);
-print('{} X 7 ='.format(n1), n1 * 7);
-print('{} X 8 ='.format(n1), n1 * 8);
-print('{} X 9 ='.format(n1), n1 * 9);
-print('{} X 10 ='.format(n1), n1 * 10);
+n1 = float(input('Digite uma distancia em metro: '));
+print('Esta distancia possue: ');
+km = n1 / 1000; hm = n1 / 100; dam = n1 / 10; dm = n1 * 10; cm = n1 * 100; mm = n1 * 1000;
+print(km); print(hm); print(dam); print(dm); print(cm); print(mm);
