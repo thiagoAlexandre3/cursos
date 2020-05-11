45bc6d990c6889af86fdba7584723e82553fcc18
thiagoAlexandre3 <thiagodev3@gmail.com>
2020-05-10 22:25:35 -0300

    Consertando mau entendido, do curso de Python, pensei que desafios era saparado de exercicios

--- curso-python/desafios/desafio-02.py
+++
@@ -1,5 +0,0 @@
-print('====== Desafio 2 ======');
-nascDia = input('Dia: ');
-nascMes = input('Mês: ');
-nascAno = input('Ano: ');
-print('Voce nasceu no dia',nascDia, 'de', nascMes, 'de', nascAno, '. Correto?');
